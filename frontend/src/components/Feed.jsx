d601430ce8e7ca48d1a3203b15c51933d92d28a3
Akash280899 <janakiakash68@gmail.com>
2022-01-18 23:10:30 +0530

    added feed component

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React , { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import MasonryLayout from './MasonryLayout'
+import Spinner from './Spinner'
+import {client} from '../client'
+import { feedQuery, searchQuery } from '../utils/data'
 
 const Feed = () => {
+    const [loading, setLoading] = useState(false);
+    const [pins, setPins] = useState()
+    const { categoryId } = useParams();
+
+    useEffect(() => {
+        setLoading(true)
+        if(categoryId) {
+            const query = searchQuery(categoryId);
+            client.fetch(query).then((data) => {
+                setPins(data)
+                setLoading(false)
+            });
+        } else {
+            client.fetch(feedQuery).then((data) => {
+                setPins(data)
+                setLoading(false)
+            })
+        } 
+    },[categoryId])
+
+    if(loading) return <Spinner message="We are looking into new ideas!!" />
+
     return (
         <div>
-            Feed
+            {pins && <MasonryLayout pins={pins} />}
         </div>
     )
 }
